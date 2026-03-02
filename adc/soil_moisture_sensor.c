ecdb1122a47ba5817cb13001fc34f220acf04c6a
Udhayakumarmurugesan <udhayapravin412@gmail.com>
2026-03-02 12:22:10 +0530

    Add files via upload

---
+++ adc/soil_moisture_sensor.c
@@ -0,0 +1,34 @@
+#include<LPC21XX.h>
+#include "lcd_n.h"
+int adc_value=0;
+float voltage=0.0;
+int ADC_Conversion()
+{
+ int ab;                                //Variable to store ADC value 
+ ADCR  = ADCR|0x01000000;               //Start conversion                    
+ while((ADDR&0x80000000)!=0x80000000);  // Wait here till conversion is over   
+ ab    = (ADDR&0x0000FFC0);             // Extracting the result               
+ ab    = (ab>>6);                       // Shift 6 bits right                  
+ voltage = (3.3*ab/1023)*100;
+ return ab;
+}
+void ADC_Check()
+{
+ADCR |= 0x00200601;                      // PDN=1,CLKDIV=6,channel=AD0.2         
+LCD_COMMAND(0x80);
+LCD_STR("ADC:");
+adc_value=ADC_Conversion();            /* Get the result of conversion         */
+LCD_COMMAND(0x85);                    /* 2nd row, 5th location                */
+LCD_INTEGER(adc_value);
+delay_ms(100);
+}
+int main()
+{
+PINSEL1 |= (1<<22); // Set P0.29 as ADC input (AD0.2)
+LCD_INIT(); 
+ while (1)
+    {
+       ADC_Check();  // Continuously check ADC value and update LCD
+    }
+}
+
